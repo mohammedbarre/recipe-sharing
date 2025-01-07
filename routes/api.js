285719a6956cd21c18546f9636e65e1ba919a106
MI_Barre <mohammedbarre17@gmail.com>
2025-01-07 01:24:27 +0000

    old api

--- routes/api.js
+++ routes/api.js
@@ -7,9 +7,10 @@ const TASTY_API_BASE_URL = 'https://tasty.p.rapidapi.com/recipes/list';
 const TASTY_API_HOST = 'tasty.p.rapidapi.com';
 const TASTY_API_KEY = '3711375463msh659873d468ffe12p1bb860jsn11e89a12800f';
 
+// Search Recipes from Tasty API
 router.get('/search-recipes', async (req, res) => {
     const query = req.query.q || ''; // Get search query from request
-    console.log(`Received search query: ${query}`); // Log the query
+
     try {
         const response = await axios.get(TASTY_API_BASE_URL, {
             params: { from: 0, size: 20, q: query },
@@ -18,14 +19,13 @@ router.get('/search-recipes', async (req, res) => {
                 'X-RapidAPI-Key': TASTY_API_KEY
             }
         });
-        console.log('API response:', response.data); // Log the API response
+
         res.json(response.data); // Return the API data as JSON
     } catch (error) {
-        console.error('Error fetching recipes from Tasty API:', error.message); // Log any errors
+        console.error('Error fetching recipes from Tasty API:', error.message);
         res.status(500).json({ error: 'Failed to fetch recipes. Please try again later.' });
     }
 });
-
 
 // Get Recipe Details by ID
 router.get('/recipe-details/:id', async (req, res) => {
